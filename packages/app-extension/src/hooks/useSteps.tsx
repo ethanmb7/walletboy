5b6598bc9431ffcc816950c08aee0642354e08ad
Karthik <105512128+karthik11135@users.noreply.github.com>
2024-05-03 12:57:27 +0530

    setState change to correct manner. update useStep.tsx
    Changed the way setState is used.

--- packages/app-extension/src/hooks/useSteps.tsx
+++ packages/app-extension/src/hooks/useSteps.tsx
@@ -2,9 +2,9 @@ import { useState } from "react";
 
 export const useSteps = () => {
   const [step, setStep] = useState(0);
-  const nextStep = () => setStep(step + 1);
+  const nextStep = () => setStep((prevStep) => prevStep + 1);
   const prevStep = () => {
-    if (step > 0) setStep(step - 1);
+    if (step > 0) setStep((prevStep) => prevStep - 1);
   };
 
   return {
